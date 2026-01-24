f032b082301889a47af5764583025719f9fba5fe
Vastalya1 <142438372+Vastalya1@users.noreply.github.com>
2026-01-25 00:01:43 +0530

    Fix collection name in checkChroma.py

--- checkChroma.py
+++ checkChroma.py
@@ -4,7 +4,7 @@ import chromadb
 client = chromadb.PersistentClient(path=r"D:\_official_\_MIT ADT_\_SEMESTER 7_\MP5\MP5-RAG-System\chromadb")
 
 # Replace 'my_collection' with the name of your collection
-collection = client.get_collection("policy_documents")
+collection = client.get_collection("policy_documentSs")
 
 # Get the first 5 documents/chunks
 results = collection.get(limit=5)
@@ -12,3 +12,18 @@ results = collection.get(limit=5)
 # Print them
 for i, doc in enumerate(results['documents']):
     print(f"Chunk {i+1}: {doc}")
+
+
+
+# collection_name = "policy_documents"
+
+# # Delete the whole collection
+# client.delete_collection(collection_name)
+
+# # (Optional) Recreate it empty
+# client.create_collection(collection_name)
+
+# collections = client.list_collections()
+
+# for c in collections:
+#     print(c.name)
